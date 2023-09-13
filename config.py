318ad1b15e99652a11a2cb96a5e697d0f33a0335
nakflix <123237114+nakflix@users.noreply.github.com>
2023-09-13 06:24:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
 
 # Heroku Credentials for updater.
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "netflixxeebot")
-HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "d188bce9-be3d-42f5-8b14-222d5cfb90d1")
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "cf493a35-2310-4162-a292-06afc8ef315e")
 
 # Custom Repo for updater.
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
