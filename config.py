7ed6df74769b3e7f1b4ef27080b563febb5ca064
nakflix <123237114+nakflix@users.noreply.github.com>
2024-01-03 11:12:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kidderke8:alpha3720@netfl
 DB_NAME = os.environ.get("DATABASE_NAME", "netflixxe")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001858872654"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001807614041"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001861505272"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001585995212"))
 
